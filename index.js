20bf35b68c6214907e598ed0fcdfbc93a94c2f86
sadiajuthi <sadia.5343@gmail.com>
2022-05-09 22:04:54 +0530

    completed

--- index.js
+++ index.js
@@ -13,32 +13,10 @@ app.use(cors());
 app.use(express.json());
 
 
-function verifyJWT(req, res, next) {
-    const authHeader = req.headers.authorization;
-    if (!authHeader) {
-        return res.status(401).send({ message: 'unauthorized access' });
-    }
-    const token = authHeader.split(' ')[1];
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-        if (err) {
-            return res.status(403).send({ message: 'Forbidden access' });
-        }
-        console.log('decoded', decoded);
-        req.decoded = decoded;
-        next();
-    })
-}
-
-
-
 // connect to database
 const { query } = require('express');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.yebql.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-
-
-
-
 
 
 // main run funtion
@@ -47,16 +25,6 @@ async function run() {
         await client.connect();
         const productCollection = client.db("bikePlanet").collection("product");
 
-
-
-        // Auth
-        app.post('/login', async (req, res) => {
-            const user = req.body;
-            const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
-                expiresIn: '1d'
-            });
-            res.send({ accessToken });
-        });
 
 
         // get all product
